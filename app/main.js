03147c76ed8eea6f116a61ddfc8724bfd5697e84
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-11-19 14:00:29 -0500

    poohb

--- app/main.js
+++ app/main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+const URL = https://openlibrary.org/search.json?q=the+lord+of+the+rings
+
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getData();

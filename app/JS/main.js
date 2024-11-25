49194d78cc39ff34438fb75538d3cbad69229169
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-11-25 13:59:48 -0500

    fghgh

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,17 +1,35 @@
 import "/CSS/style.css";
-const URL = "https://api.edamam.com/doc/open-api/recipe-search-v2.json"
 
-async function getData(URL) {
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        console.log(data);
-    } catch(error) {
-        console.log(error);
-    }
+const recipes_url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '11bed04c27mshb6f8302c8454cabp10afc7jsnc701adc4abf2',
+		'x-rapidapi-host': 'tasty.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(recipes_url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
 }
 
-function pushdata() {
-    const container = document.querySelector("api-data");
-    }
-pushdata();
+const tags_url = 'https://tasty.p.rapidapi.com/tags/list';
+const choices = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '11bed04c27mshb6f8302c8454cabp10afc7jsnc701adc4abf2',
+		'x-rapidapi-host': 'tasty.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(tags_url, choices);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

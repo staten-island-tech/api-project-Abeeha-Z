a4e742724f06ad86a1766ea87cc3eb0d42f3f163
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-11-27 13:05:27 -0500

    df

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,6 @@
 import "/CSS/style.css";
-
-const recipes_url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+const url = 'https://tasty.p.rapidapi.com/recipes/list';
+container = document.querySelector("#recipe_container");
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,26 +10,22 @@ const options = {
 };
 
 try {
-	const response = await fetch(recipes_url, options);
+	const response = await fetch(url, options);
 	const result = await response.text();
 	console.log(result);
 } catch (error) {
 	console.error(error);
 }
 
-const tags_url = 'https://tasty.p.rapidapi.com/tags/list';
-const choices = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '11bed04c27mshb6f8302c8454cabp10afc7jsnc701adc4abf2',
-		'x-rapidapi-host': 'tasty.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(tags_url, choices);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+container.insertAdjacentHTML (
+	"beforeend",
+    `
+      <div class="recipe">
+      <img src="${recipe.thumbnail_url}" class="images">
+      <h2>${recipe.name}</h2>
+      <div class="item-properties">
+      <p>${recipe.description}</p>
+      </div>
+  </div>
+`
+)

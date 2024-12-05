efa948d6f212da5f9cb0f6515565fcb5e13c10c6
Abeeha-Z <abeehaz@nycstudents.net>
2024-12-04 21:20:19 -0500

    kjhjbj

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,4 +1,4 @@
-const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=40";
+const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=100";
 const options = {
   method: "GET",
   headers: {
@@ -18,36 +18,20 @@ async function getData() {
   }
 }
 
-getData();
-
-const container = document.getElementById("recipes");
+const container = document.getElementById("recipe-list");
 const showRecipes = async () => {
   // defining an async arrow function
   const recipes = await getData(url);
   console.log("Data Found");
   for (let i = 0; i < recipes.results.length; i++) {
-    console.log("works");
+    //if recipe has a thumbnail, url and description
     container.innerHTML += `
     <div>
     Name: ${recipes.results[i].name}<br>
     Description: ${recipes.results[i].description}<br>
-    <img src='${recipes.results[i].thumbnail_url}'>
     </div>
     `;
   }
 };
 
 showRecipes();
-
-// container.insertAdjacentHTML(
-//   "beforeend",
-//   `
-//       <div class="recipe">
-//       <img src="${recipe.thumbnail_url}" class="images">
-//       <h2>${recipe.name}</h2>
-//       <div class="item-properties">
-//       <p>${recipe.description}</p>
-//       </div>
-//   </div>
-// `
-// );
